062274801fa8ff24cb1c8ccac8f7c368582455c0
xlplbo <booxlp@gmail.com>
2014-10-23 01:37:36 +0800

    修改:         .vimrc 	修改:         1.h 	修改:         cdkey.h

--- 1.h
+++ 1.h
@@ -1,6 +1,5 @@
 #include <algorithm>
 #include <cstdlib>
-
 //1.1
 //给定字符串是否有重复字符
 bool IsUniqueString(const char * des, int nSize)
@@ -14,7 +13,6 @@ bool IsUniqueString(const char * des, int nSize)
     }
     return true;
 }
-
 //1.2
 //翻转字符串
 void ReverseString(char * des, int nSize)
@@ -28,23 +26,18 @@ void ReverseString(char * des, int nSize)
         des[nSize - 1 - i] = tempChar;
     }
 }
-
 //1.3
 //比较两个字符串是否为变位词
 bool IsPermutation(std::string &a, std::string &b)
 {
     if (a.length() != b.length())
         return false;
-
     sort(a.begin(), a.end());
     sort(b.begin(), b.end());
-
     if (a != b)
         return false;
-
     return true;
 }
-
 //1.8
 //是否为旋转字符
 bool IsRatation(std::string a, std::string b)
@@ -56,8 +49,6 @@ bool IsRatation(std::string a, std::string b)
 		return true;
 	return false;
 }
-
-
 int CountZero(int n)
 {
 	unsigned long long nValue = 1;
@@ -73,7 +64,6 @@ int CountZero(int n)
 	}
 	return nCount;
 }
-
 int CountZero2(int n)
 {
 	int count = 0;
@@ -83,27 +73,21 @@ int CountZero2(int n)
 		count += n / i;
 	return count;
 }
-
 int MyRand(int low, int high)
 {
 	return low + rand() % (high - low + 1);
 }
-
 int* shuffle(int* cards, int n)
 {
 	if (n <= 0)
 		return cards;
-
 	shuffle(cards, n - 1);
 	int rand = MyRand(0, n);
-
 	int temp = cards[rand];
 	cards[rand] = cards[n];
 	cards[n] = temp;
-
 	return cards;
 }
-
 void shuffle2(int* cards, int n)
 {
 	// 随机i-1个的任意一个数与i交换
@@ -115,32 +99,27 @@ void shuffle2(int* cards, int n)
 		cards[i] = temp;
 	}
 }
-
 void* aligned_malloc(size_t size, size_t alignd_byte)
 {
 	//offset地址偏移量(byte)
 	//alignd_byte对齐字节的预分配空间
 	//sizeof(void *)保存真实指针的预分配空间
 	size_t offset = sizeof(void *) + alignd_byte - 1;
-
 	//预分配更大的内存块
 	//q指向这块内存的首地址
 	void* q = malloc(size + offset);
 	if (!q)
 		return NULL;
 	//printf("q = 0x%p\n", q);
-
 	//对齐后的内存块
 	//不管怎样q指针都向后偏移，再& ~(alignd_byte - 1)地址对齐
 	void* p = (void *)(((size_t)(q)+offset) & ~(alignd_byte - 1));
 	//为了配合free函数，保存q指针到p-1的位置
 	//p[-1] = q;直接这样做不行void *大小未知无法寻址
 	*(((void **)p) - 1) = q;
-
 	//返回对齐后的指针
 	return p;
 }
-
 void aligned_free(void* p)
 {
 	//计算原内存块的首地址
--- cdkey.h
+++ cdkey.h
@@ -1,15 +1,12 @@
 #include <string.h>
 #include <time.h>
 #include <stdlib.h>
-
 //cdkey
 #define MAX_MASK_LEN		5
 #define MAX_PASS_LEN		(2 * MAX_MASK_LEN)
 #define CK_ISNUM(c)			(c >= '0' && c <= '9')
 #define CK_ISCHAR(c)		(c >= 'A' && c <= 'Z')
-
 const char* CK_MASK_TABLE = "3ZOEA5UI4FP2BQGC7SVHD9W1KRM0TX8LJNY6";
-
 inline void cdkeyGenMask(char* szMask)
 {
 	int nSize = strlen(CK_MASK_TABLE);
@@ -21,17 +18,14 @@ inline void cdkeyGenMask(char* szMask)
 	}
 	szMask[MAX_MASK_LEN] = '\0';
 }
-
 inline void cdkeySwapChars(char* sz)
 {
 	char c;
 #define CDKEY_SWAP(n1, n2)	c = sz[n1]; sz[n1] = sz[n2]; sz[n2] = c
-
 	CDKEY_SWAP(0, 9);
 	CDKEY_SWAP(1, 6);
 	CDKEY_SWAP(3, 8);
 }
-
 inline int cdKeyLocateIndex(char c)
 {
 	for (size_t i = 0; i < strlen(CK_MASK_TABLE); i++)
@@ -43,15 +37,12 @@ inline int cdKeyLocateIndex(char c)
 	}
 	return -1;
 }
-
 inline int cdkeyEncrypt(char* szResult, const char* szPass)
 {
 	if (!szResult || !szPass || strlen(szPass) != MAX_MASK_LEN)
 		return 0;
-
 	char szMask[MAX_MASK_LEN + 1] = "";
 	cdkeyGenMask(szMask);
-
 	int nMaskIndex = -1;
 	int nPassIndex = -1;
 	int nSize = strlen(CK_MASK_TABLE);
@@ -71,7 +62,6 @@ inline int cdkeyEncrypt(char* szResult, const char* szPass)
 	cdkeySwapChars(szResult);
 	return 1;
 }
-
 inline int	cdkeyDecrypt(char* szResult, const char* szEncrypted)
 {
 	if (!szEncrypted || strlen(szEncrypted) != MAX_PASS_LEN)
@@ -79,7 +69,6 @@ inline int	cdkeyDecrypt(char* szResult, const char* szEncrypted)
 	char szPass[MAX_PASS_LEN + 1] = "";
 	strncpy_s(szPass, szEncrypted, sizeof(szPass));
 	cdkeySwapChars(szPass);
-
 	int nMaskIndex = -1;
 	int nPassIndex = -1;
 	int nSize = strlen(CK_MASK_TABLE);
