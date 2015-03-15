a685997193ab19e4c259aa80317b3e7ded51b8c8
xlplbo <booxlp@gmail.com>
2015-03-15 18:13:24 +0800

    自动化配置工具

--- autoconfig/autoconfig.py
+++ autoconfig/autoconfig.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # coding:utf-8
 # Contributor:
-# 	liubo5 <lboxlp@163.com>
+# liubo5 <lboxlp@163.com>
 
 __version__ = '1.0'
 
@@ -9,21 +9,26 @@ import os
 import sys
 from optparse import OptionParser
 import zipfile
+import codecs
 import ConfigParser
 import logging
+import shutil
 
 RC_LOG_FILE = "autoconfig.log"
 RC_CONFIG_FILE = "config.ini"
 RC_WINDOW_PACK = "library.zip"
 RC_COMMON_PACK = "data.zip" 
 RC_EXT_OR_ELF = ('.dll', '.exe', ".so", ".a", 'kg_goddess', 'kg_bishop', 'so2relay', 'so2gamesvr')
+RC_CLIENT_LIST = ('curl.exe', 'engine.dll', 'lualibdll.dll', 'represent3.dll', 'sound.dll', 
+			'verify_up2date.exe', 'so2game.exe', 'heaven.dll', 'rainbow.dll')
 
 class AutoConfig(object):
 	"""docstring for AutoConfig"""
 	def __init__(self, config, log):
 		super(AutoConfig, self).__init__()
+		self.config = config
 		self.configParser = ConfigParser.ConfigParser()
-		self.configParser.read(config)
+		self.configParser.readfp(codecs.open(config, "r", "utf-8-sig"))  
 		self.log = logging.getLogger(__file__)
 		self.log.setLevel(logging.INFO)
 		formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
@@ -84,7 +89,7 @@ class AutoConfig(object):
 		self.log.info("Prepare to packaged programs and librarys!")
 		self.log.warn("please compile your project at first, and then run packaged program!")
 		if self._checkFileExist(name):
-			if self._checkYesOrNO("check file '%s' is exist! do you update this file?(y/N)" %name):
+			if self._checkYesOrNO("check file '%s' is exist! do you update this file?(y/N):" %name):
 				self.log.info("remove '%s' ..." %name)
 				os.remove(name)
 			else:
@@ -113,8 +118,85 @@ class AutoConfig(object):
 		self.log.info("generate '%s' success! " %name)
 		self.log.info("packaged programs and librarys finished!")
 
+	def _moveFolder(self, src, dst):
+		if not os.path.exists(dst):
+			os.mkdir(dst)
+		for name in os.listdir(src):
+			if '.svn' in name.strip():
+				continue
+			srcname = os.path.join(src, name)
+			dstname = os.path.join(dst, name)
+			try:
+				if os.path.islink(srcname):
+					linkto = os.readlink(srcname)
+					os.symlink(linkto, dstname)
+				elif os.path.isfile(srcname):
+					if os.path.isdir(dstname):
+						os.rmdir(dstname)
+					elif os.path.isfile(dstname):
+						os.remove(dstname)
+					shutil.copy2(srcname, dstname)
+					shutil.copystat(srcname, dstname)
+				else:
+					self._moveFolder(srcname, dstname)
+			except Exception, e:
+				self.log.error(str(e))
+
+	def _extractWinLibrary(self, path):
+		self.log.info("copy windows librarys to '%s'" %path)
+		zipFile = zipfile.ZipFile(RC_WINDOW_PACK, 'r', zipfile.ZIP_DEFLATED)
+		for file in zipFile.namelist():
+			zipFile.extract(file, path)
+			self.log.info("extract '%s' ..." %file)
+
+	def _extractLibrary(self, path, list):
+		self.log.info("copy programs and librarys to '%s'" %path)
+		zipFile = zipfile.ZipFile(RC_COMMON_PACK, 'r', zipfile.ZIP_DEFLATED)
+		extFile = []
+		for file in zipFile.namelist():
+			lfile = file.lower()
+			lfiled = lfile.replace('d.', '.')
+			if lfile in list:
+				zipFile.extract(file, path)
+				extFile.append(lfile)
+				self.log.info("extract '%s' ..." %file)
+			elif lfiled in list:
+				zipFile.extract(file, path)
+				extFile.append(lfiled)
+				self.log.info("extract '%s' ..." %file)
+		for file in list:
+			if file not in extFile:
+				self.log.error("can not find '%s', please check '%s' file" %(file, RC_COMMON_PACK))
+
 	def configClient(self):
-		pass
+		self.log.info("Prepare to config client!")
+		if self._isLinux() and not self._checkYesOrNO("do you config client on linux OS?(y/N):"):
+			self.log.info("config client on linux OS cancel by user!")
+			return False
+		dstPath = self.configParser.get("client", "home").strip()
+		if os.path.isdir(dstPath):
+			self.log.info("remove dir '%s' ..." %dstPath)
+			shutil.rmtree(dstPath)
+		try:
+			self.log.info("mkdir '%s' ..." %dstPath)
+			os.makedirs(dstPath)
+		except Exception, e:
+			self.log.error(str(e))
+			return False
+		resource = self.configParser.get("client", "resource").strip()
+		if not os.path.isdir(resource):
+			self.log.error("please check '%s' client config, because '%s' is not existed!@@@@@@@" %(self.config, resource))
+			return False
+		catalog = self.configParser.get("client", "catalog").split(',')
+		for clv in catalog:
+			clvpath = os.path.join(resource, clv)
+			if not os.path.exists(clvpath):
+				self.log.error("please check '%s' client config, because '%s' is not existed!########" %(self.config, resource))
+				return False
+			self.log.info("move floders '%s' to '%s' ..." %(clvpath, dstPath))
+			self._moveFolder(clvpath, dstPath)
+		self._extractWinLibrary(dstPath)
+		self._extractLibrary(dstPath, RC_CLIENT_LIST)
 
 	def configServer(self):
 		pass
