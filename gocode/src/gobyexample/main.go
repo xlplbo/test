bb56df761a1f404f601208397d031f1e28a13e7e
unknown <booxlp@gmail.com>
2016-05-05 16:02:16 +0800

    go profile test

--- gocode/src/gobyexample/main.go
+++ gocode/src/gobyexample/main.go
@@ -1,53 +1,46 @@
 package main
 
+import (
+	"fib"
+	"github.com/pkg/profile"
+	"time"
+	//"flag"
+	//"log"
+	//"os"
+	//"runtime/pprof"
+)
+
+//var cpuprofile = flag.String("cpuprofile", "cpu.prof", "write cpu profile to file")
+
+func memtest(exit chan<-int) {
+	tmp := make([]uint32, 1000000)
+	for kk, _ := range tmp {
+		tmp[kk] = 0
+	}
+	tmp = append(tmp, 1)
+	println(len(tmp), cap(tmp))
+	time.Sleep(5 * time.Second)
+	exit <- 1
+}
+
 func main() {
-	//helloworld()
-	//values()
-	//variable()
-	//constant()
-	//For()
-	//ifelse()
-	//Switch()
-	//Array()
-	//slice()
-	//Map()
-	//Range()
-	//Function()
-	//MultiReturn()
-	//VariableParam()
-	//Closure()
-	//Recursive()
-	//pointer()
-	//Struct()
-	//Interface()
-	//ErrorHandling()
-	//TestGoRoutine()
-	//Channel()
-	//Channel2()
-	//Channel3()
-	//Channel4()
-	//Channel5()
-	//Channel6()
-	//Select()
-	//Timer()
-	//Ticker()
-	//WorkPool()
-	//Atomic()
-	//Mutex()
-	//RoutineState()
-	//Sort()
-	//UseSort()
-	//Panic()
-	//Defer()
-	//CombinedFunc()
-	//testString()
-	//testFormat()
-	//testRegexp()
-	//testJson()
-	//testUrl()
-	//testSha1()
-	//testBase64()
-	//testFlag()
-	//testCommad()
-	testSignal()
+	defer profile.Start(profile.MemProfile).Stop()
+	//os.Setenv("GODEBUG", "gctrace=1")
+	// for _, v := range os.Environ() {
+	// 	println(v)
+	// }
+	// flag.Parse()
+	// f, err := os.Create(*cpuprofile)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// pprof.StartCPUProfile(f)
+	// defer pprof.StopCPUProfile()
+	exit := make(chan int)
+	go memtest(exit)
+	println(fib.Fib(40))
+	println(fib.Fib2(40))
+	println(fib.Fib3(40))
+	<-exit
+	return
 }
